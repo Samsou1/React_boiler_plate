cff9378cf76305dfc8dbf40e3e5394617e6f78cc
Samsou1 <sami.linard.pro@gmail.com>
2022-11-23 17:05:37 +0100

    jotai

--- App.jsx
+++ App.jsx
@@ -8,7 +8,6 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Navbar from './components/Navbar'
 import Login from './pages/Login';
-import Logout from './pages/Logout';
 import Register from './pages/Register';
 
 function App() {
@@ -20,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route element={<PrivateRoute />}>
             <Route path='/about' element={<About />} />
+          <Route path='/register' element= {<Register />} /> 
           </Route>
         </Routes>
       </main>
--- pages/Register/index.jsx
+++ pages/Register/index.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import APIManager from "../../services/api"
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
+  const [_, setUser] = useAtom (userAtom)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+   
+  
+    const data = {
+      user: {
+          email: email,
+          password: password
+      }
+  }
+  
+    try {
+      const response = await APIManager.register(data);
+      setUser(response.data.user)
+      console.log(response)
+    } catch(err) {
+  console.error(err)
+    }
+    
+  
+  }
+  
+    return (
+      <>
+      <h1 className="title-form">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="container-form">
+        <label htmlFor="email">Email </label>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          type="text"
+          id="email"
+          placeholder="email"
+          />
+         
+        <label htmlFor="password">Password</label>
+        
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="text"
+          id="password"
+          placeholder="password"
+          />
+   
+         
+      
+        <button>Sign Up</button>
+      </form>
+    </>
+    )
+  }
+  
 
 export default Register
