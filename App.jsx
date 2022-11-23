78b806c6a79e5060551d5f233abb84b612005c62
Samsou1 <sami.linard.pro@gmail.com>
2022-11-23 15:52:47 +0100

    routing works

--- App.jsx
+++ App.jsx
@@ -1,10 +1,26 @@
-import './App.css'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import PrivateRoute from './config/PrivateRoute'
+import Home from './pages/Home'
+import About from './pages/About'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <main>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<PrivateRoute />}>
+            <Route path='/about' element={<About />} />
+          </Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
--- services/api.js
+++ services/api.js
@@ -1,3 +1,8 @@
+import axios from "axios";
+import Cookies from "js-cookie"
+
+
+
 const API = axios.create({ baseURL: 'http://localhost:3000' });
 
 API.interceptors.request.use(({ headers, ...config }) => ({
@@ -8,3 +13,17 @@ API.interceptors.request.use(({ headers, ...config }) => ({
         Authorization: `Bearer ${headers.Authorization ||  Cookies.get('token')}`,
     },
 }));
+
+export default class APIManager {
+    static async registerUser(data) {
+        const response = await API.post('/auth/register', data);
+        Cookies.set("token", response.headers.authorization)
+        return response.data;
+    }
+
+    static async LoginUser (data) {
+        const response = await API.post("/users/sign_in", data);
+        Cookies.set("token", response.headers.authorization)
+        return response.data
+    }
+}
